b6928e231756db81caa536ef8335a6526c4a9a67
ckriutz <casey.kriutzfield@live.com>
2022-05-24 16:19:12 -0400

    Added the county JSON file and will read from that instead.

--- src/components/registerdata.js
+++ src/components/registerdata.js
@@ -1,19 +1,12 @@
 import React from "react"
 import Counter from "./counter"
+import JSONData from "../../src/data/unionCountyData.json"
 
-var nmbrs = 1033;
-
-fetch('https://libertyfunctions.azurewebsites.net/api/RegisteredAPIFunction')
-.then(response => response.json())
-.then((bodyData) => {
-  // jsonData is parsed json object received from url
-  console.log(bodyData);
-  nmbrs = bodyData;
-});
+//var nmbrs = 1033;
 
 const RegistrationData = (props) => (
   <div className="registrationdata">
-    Registered Libertarians in Union County: <Counter>{nmbrs}</Counter>
+    Registered Libertarians in Union County: <Counter>{JSONData.Libertarians}</Counter>
   </div>
 )
 
