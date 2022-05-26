9277e433372aa238652cab9f9f168d832f25d8cd
ckriutz <casey.kriutzfield@live.com>
2022-05-26 13:22:49 -0400

    updated to attempt to run from script.

---
+++ .github/workflows/scraper.ps1
@@ -0,0 +1,37 @@
+﻿# First, lets create the date.
+$daysAway = [Int] (get-date).DayOfWeek + 1
+$saturday = (Get-Date).AddDays(-$daysAway)
+
+$seperator = "%2F"
+
+if([Int] $saturday.Month -lt 10)
+{
+    $monthNumber = "0" + $saturday.Month
+}
+else
+{
+    $monthNumber = $saturday.Month
+}
+
+if([Int] $saturday.Day -lt 10)
+{
+    $dayNumber = "0" + $saturday.Day
+}
+
+else
+{
+    $dayNumber = $saturday.Day
+}
+
+$dateString = "$monthNumber$seperator$dayNumber$seperator$([Int] $saturday.Year)"
+
+# Now, lets get the data and parse it.
+$uri = "https://vt.ncsbe.gov/RegStat/Results/?date=$dateString"
+$html = Invoke-WebRequest -Uri $uri
+$data -match "var data = (.*?)]"
+$jsonData = $Matches[0] -replace "var data = ", ''
+$countyData = ConvertFrom-Json $jsonData
+$unionCountryData = $countyData | Where-Object {$_.CountyName -eq "UNION"}
+$unionCountryData | ConvertTo-Json | Out-File "../../src/data/unionCountyData.json"
+
+echo $unionCountryData
